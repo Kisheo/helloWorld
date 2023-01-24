6209200c9f015bb4d83b7e644b6ec9deca7c616e
Inno <inokisheo@gmail.com>
2023-01-24 18:14:15 +0300

    comment was added

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,71 @@
+fun main(arg:Array<String>){
+   // println("Hi \"mama\" \nThe mama was in an escaped quotes")
+/*    var age =25
+    age=26
+    val newAge= age+3*/
+
+/*
+//    newAge=45
+  //  println(newAge)
+*/
+
+val catName = "My cat's name is \"Fluffy\""
+/*    println("\\ and /")
+    println("$catName")
+    println("$catName and it is ${34+age} years old")
+    println(catName.length)*/
+/*
+    var myCar="my car won't start"
+    //println(myCar.substring(3, 6))
+    var clientName ="Marry"
+    var numberOfitems =3
+    var price= 30
+   println("Hellow $clientName, thank you for doing business with us. your total Ammount is ${numberOfitems*price}$ ")
+*/
+
+/* */   print("What is your name?\n")
+    val usserInput= readLine()
+    println("Hellow $usserInput, Welcome onboard.\nPlease enter your Number")
+    val usserInput2= readLine()?.toInt()?.times(5)
+    println("your number Times 5 is equals to: $usserInput2")
+
+//print(catName::length)
+//comment added
+//    operators assignment, interest
+/*
+println("Enter Current Account balance in Tsh.")
+    var inputi= readLine()?:"0"
+    var balance =inputi.toDouble()
+    var rate:Double = 5.5/100
+        ...................
+*/
+/*var inno: Int? =10
+    println(inno?.times(34))
+    if (inno != null) {
+        println(inno*34)
+    }
+var yeye:String? ="mamaeeeeeh!"
+    println(yeye?.substring(3,6))*/
+/*    var dbl:Double=5.58
+    println(dbl?.times(6)?.toFloat())
+
+    var name= readLine()
+    println(name?.length)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
